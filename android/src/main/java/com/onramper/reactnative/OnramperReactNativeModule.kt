e20ff7573ea32d548afda0de0ec04471f16c32b1
Egor Bogdanov <egor@onramper.com>
2026-05-20 17:04:44 +0200

    feat(android): platformUnsupported stub module

--- android/src/main/java/com/onramper/reactnative/OnramperReactNativeModule.kt
+++ android/src/main/java/com/onramper/reactnative/OnramperReactNativeModule.kt
@@ -1,50 +1,33 @@
 package com.onramper.reactnative
 
+import expo.modules.kotlin.exception.CodedException
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
-import java.net.URL
+
+class PlatformUnsupportedException :
+    CodedException("Onramper React Native is iOS-only in this release.") {
+    override fun getCode(): String = "platformUnsupported"
+}
 
 class OnramperReactNativeModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
-  override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('OnramperReactNative')` in JavaScript.
-    Name("OnramperReactNative")
+    override fun definition() = ModuleDefinition {
+        Name("OnramperReactNative")
+        Events("onStateChanged", "onCheckoutEvent")
 
-    // Defines constant property on the module.
-    Constant("PI") {
-      Math.PI
+        AsyncFunction("configure") { _: Map<String, Any?>, _: Any? ->
+            throw PlatformUnsupportedException()
+        }
+        AsyncFunction("initialize") { _: String, _: String ->
+            throw PlatformUnsupportedException()
+        }
+        AsyncFunction("getCheckoutRequirements") { _: Map<String, Any?>, _: Map<String, Any?> ->
+            throw PlatformUnsupportedException()
+        }
+        AsyncFunction("cancelPreparedIntent") { _: String ->
+            throw PlatformUnsupportedException()
+        }
+        AsyncFunction("reset") {
+            throw PlatformUnsupportedException()
+        }
     }
-
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
-    }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(OnramperReactNativeView::class) {
-      // Defines a setter for the `url` prop.
-      Prop("url") { view: OnramperReactNativeView, url: URL ->
-        view.webView.loadUrl(url.toString())
-      }
-      // Defines an event that the view can send to JavaScript.
-      Events("onLoad")
-    }
-  }
 }
